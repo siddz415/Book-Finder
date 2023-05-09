baecbfeebd84a655fe8f98defee12e56527edf93
Sidd <siddz415@gmail.com>
2023-05-09 15:33:04 -0700

    deleted api.js and added mutation.js and queries.js in client folder also added schema folder and deleted controller folder in server

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,83 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+  mutation addUser( $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        email
+        username
+        bookCount
+        savedBooks {
+          authors
+          link
+          title
+          bookId
+          image
+          description
+        }
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login( $email: String!
+    $password: String!
+  ) {
+    login(
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        username
+        _id
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: InputBook!) {
+    saveBook(newBook: $newBook) {
+      _id
+      email
+      username
+      savedBooks {
+        bookId
+        authors
+        title
+        link
+        description
+        image
+      }
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation deleteBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      email
+      username
+      savedBooks {
+        bookId
+        authors
+        link
+        title
+        description
+        image
+      }
+    }
+  }
+`;
